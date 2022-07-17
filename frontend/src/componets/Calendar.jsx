e5a6446717646cc56ac668345a9e4d246959746f
Fernando Castro <fernandocas.972@gmail.com>
2022-07-17 18:58:09 -0500

    update

--- frontend/src/componets/Calendar.jsx
+++ frontend/src/componets/Calendar.jsx
@@ -10,7 +10,7 @@ export default function Calendar(){
     return(
         <div>
             <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin,interactionPlugin]}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView="dayGridMonth"
                 headerToolbar={{
                     left: "prev,next today",
@@ -24,8 +24,8 @@ export default function Calendar(){
                 }}
                 timeZone="local"
                 events={[
-                    {title: "event 1", start: "2022-07-18T12:30:00", end: "2022-07-18T13:30:00"},
-                    {title: "event 2", start: "2022-07-18T14:30:00", end: "2022-07-18T16:30:00",nombre_participante:"Pedro"}
+                    {title: "Cita 1", start: "2022-07-18T12:30:00",  end: "2022-07-18T13:30:00", backgroundColor: "red", borderColor: "red", nombre_participante:"Sebas" },
+                    {title: "Cita 2", start: "2022-07-18T14:30:00", end: "2022-07-18T16:30:00",nombre_participante:"Pedro"}
                 ]}
                 eventClick={handleEventClick}
                 eventContent={renderEventContent}
@@ -37,9 +37,6 @@ export default function Calendar(){
 function renderEventContent(eventInfo){
     return(
         <div>
-            <div>
-                Cita
-            </div>
             <div>
                 {eventInfo.event.title}
             </div>
